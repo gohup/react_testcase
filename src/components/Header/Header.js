25c29fabdc41789df08b8f49518c3057d3cb36c2
gohup <answer42@xs4all.nl>
2016-08-21 15:43:02 +0200

    change head

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,7 @@ import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-<<<<<<< HEAD
-    <h1>React Redux Master changed by remote</h1>
-=======
-    <h1>React Redux Master changed by remote</h1>
->>>>>>> master
+    <h1>React Redux Master changed local branch</h1>
     <IndexLink to='/' activeClassName={classes.activeRoute}>
       Home
     </IndexLink>
